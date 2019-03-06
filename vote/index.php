bec0dc4bd9ddc6ab807091e9cca2a22292298704
bienenprojekt <bienenprojekt.sfn@gmail.com>
2019-03-06 19:47:50 +0100

    remove references to old host

--- vote/index.php
+++ vote/index.php
@@ -1,7 +1,1 @@
-<?php
-
-header('Location: http://sfn.afcarl.de#greentec', true, 301); exit;
-
-?>
-
-<meta http-equiv="refresh" content="0; url=http://sfn.afcarl.de#greentec" />
+Voting has ended. Thank you for participating!
